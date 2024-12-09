2c5db77ed7d98f0f657f9a39306185e84df49a64
krishnapandey0192 <krishnapandey0206@gmail.com>
2024-12-09 12:02:16 +0530

    connect to frontend url for improve cors error

--- server/main.js
+++ server/main.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3001;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-bot-gemini-phi.vercel.app/",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     // allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
